1e9a8b0b31867f7cd1168761fb89074ed8356c2a
Akilah Littlejohn <akilahlittlejohn@gmail.com>
2020-09-15 16:05:46 -0400

    Site colors, nav refactored, angular-material card, layout changes

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -12,6 +12,11 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+
+
 
 @NgModule({
   declarations: [
@@ -29,6 +34,9 @@ import { MatButtonModule } from '@angular/material/button';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
