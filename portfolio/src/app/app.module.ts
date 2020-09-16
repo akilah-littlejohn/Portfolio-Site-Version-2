238465cb69d57e55d8911b6c13571c26ba260be6
Akilah Littlejohn <akilahlittlejohn@gmail.com>
2020-09-16 17:54:00 -0400

    style prototype need to refactor

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -11,12 +11,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { FooterComponent } from './footer/footer.component';
-
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -39,6 +38,7 @@ import { FooterComponent } from './footer/footer.component';
     MatToolbarModule,
     MatTableModule,
     MatCardModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent],
